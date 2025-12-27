98e4d6c851829f6c2bcff8b532274ca3409fd836
torexbtc234 <emmanuelsomoyo@gmail.com>
2025-12-27 12:35:22 +0100

    Change WebSocket port from 8000 to 3000

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ BOT_TOKEN = "7745488293:AAGz9DLl2jtw1YvHp3lIsCg1vYE3ZO3vWjk"
 GROUP_ID = --1003401003553
 
 # Default WebSocket port (Deployra uses $PORT)
-WS_PORT = 8000
+WS_PORT = 3000
 
 # Temporary directory for visitor voice/files
 TMP_DIR = "/tmp/livechat_files"
