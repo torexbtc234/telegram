23f0713ff3165c5a5f0ea8a4200b9f206576db54
torexbtc234 <emmanuelsomoyo@gmail.com>
2025-12-27 12:31:21 +0100

    Correct GROUP_ID format in config.php
    Fix GROUP_ID format by removing extra hyphen.

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 BOT_TOKEN = "7745488293:AAGz9DLl2jtw1YvHp3lIsCg1vYE3ZO3vWjk"
 
 # Telegram group ID (replace with your admin group ID)
-GROUP_ID = -1003401003553
+GROUP_ID = --1003401003553
 
 # Default WebSocket port (Deployra uses $PORT)
 WS_PORT = 8000
